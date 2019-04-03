50f6a897f20f0345192a2ac9b4df28f391f27e79
Joshua Vance <skinsfandc0612@icloud.com>
2019-04-03 12:36:20 -0400

    updated index.js to reflect models

--- models/index.js
+++ models/index.js
@@ -2,5 +2,7 @@ const mongoose = require('../db/connection');
 
 module.exports = {
   User: mongoose.model('User', require('./user')),
+  loocation: mongoose.model('Loocation', require('./loocation')),
+  Loocator: mongoose.model('Loocator', require('./loocator')),
   Humor: mongoose.model('Humor', require('./humor')),
 };
