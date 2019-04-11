6aaed9f0ef32136df6407bcfb0125c0ec95f903d
Joshua Vance <skinsfandc0612@icloud.com>
2019-04-11 19:58:11 -0400

    updated lat and lng for loocation seed

--- db/loocationSeed.js
+++ db/loocationSeed.js
@@ -2,9 +2,17 @@ const { Loocation } = require('../models/index');
 
 Loocation.remove({}).then(_ => {
   Loocation.create({
-    Name: 'Starbucks',
-    lat: '38.836104',
-    lng: '-77.15649789999999',
+    name: 'Starbucks',
+    location: {
+      lat: '38.836104',
+      lng: '-77.15649789999999',
+    },
+    misc: 'This is the cleanest bathroom I have ever seend',
+    up_vote: {
+      type: '3',
+      default: 0,
+    },
+    author: 'Jvance963',
   }).then(loocation => {
     console.log(loocation);
     process.exit();
