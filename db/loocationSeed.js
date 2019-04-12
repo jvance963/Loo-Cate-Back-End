c8ed12aaea139425107ed6938deebbaa3670f984
Joshua Vance <skinsfandc0612@icloud.com>
2019-04-11 20:00:18 -0400

    updated lat and lng for loocation seed

--- db/loocationSeed.js
+++ db/loocationSeed.js
@@ -8,11 +8,6 @@ Loocation.remove({}).then(_ => {
       lng: '-77.15649789999999',
     },
     misc: 'This is the cleanest bathroom I have ever seend',
-    up_vote: {
-      type: '3',
-      default: 0,
-    },
-    author: 'Jvance963',
   }).then(loocation => {
     console.log(loocation);
     process.exit();
